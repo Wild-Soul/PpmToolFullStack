5661dddbdc7eb4d0d40edfc43a1b33620a22c552
Wild-Soul <rohitkk074@gmail.com>
2022-10-06 21:37:22 +0530

    Added global exception handler and refactored controller.

--- Server/src/main/java/io/spring/starterproject/services/ProjectService.java
+++ Server/src/main/java/io/spring/starterproject/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.spring.starterproject.services;
 
 import io.spring.starterproject.domain.Project;
+import io.spring.starterproject.exceptions.ProjectIdException;
 import io.spring.starterproject.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,16 @@ public class ProjectService {
     ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException(
+                String.format(
+                    "Project ID %s already exists!",
+                    project.getProjectIdentifier().toUpperCase()
+                )
+            );
+        }
     }
 }
