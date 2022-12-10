18786338184ebab107aa190bb5b1b94d3b663b36
Wild-Soul <rohitkk074@gmail.com>
2022-12-10 22:40:19 +0530

    React-redux setup

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,27 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Layout/Navbar";
 import AddProject from "./components/Project/AddProject";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route exact path="/addProject" element={<AddProject />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
